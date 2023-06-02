f89d47826f0f05c7e1089410802aaee94c7a4fc2
Steve <sjd734312@gmail.com>
2023-06-02 16:10:10 -0400

    my first objects lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Edward',
+    streetAddress: '7 pearl'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    'Name',
+    'Sam'
+)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+const firedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Sam');
+
+function deleteFromEmployeeByKey(obj, key){
+    return {
+        ...obj[key],
+    }
+}
+
+const deletedEmployee = deleteFromEmployeeByKey(firedEmployee, "Sam");
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+     delete obj[key];
+
+     return obj;
+}
+
+deleteFromEmployeeByKey(employee, "name");
